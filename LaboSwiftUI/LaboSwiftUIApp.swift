ea26ad1bd380f177759a20cfbd3ed2f9b59550ab
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-04-10 23:17:33 +0900

    Initial commit

---
+++ LaboSwiftUI/LaboSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaboSwiftUIApp.swift
+//  LaboSwiftUI
+//
+//  Created by Ken Tsutsumi on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct LaboSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
