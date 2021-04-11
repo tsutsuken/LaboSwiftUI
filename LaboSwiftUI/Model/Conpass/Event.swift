e48274afb9ea2e2549a5fe423753985b0ae99795
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2021-04-11 23:16:40 +0900

    Fetch events on connpass

---
+++ LaboSwiftUI/Model/Conpass/Event.swift
@@ -0,0 +1,28 @@
+//
+//  Event.swift
+//  LaboSwiftUI
+//
+//  Created by Ken Tsutsumi on 2021/04/11.
+//
+
+import Foundation
+
+struct Event: Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var eventUrl: String
+    var hashTag: String
+    var startDate: String
+    var address: String
+    var ownerDisplayName: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "event_id"
+        case title = "title"
+        case eventUrl = "event_url"
+        case hashTag = "hash_tag"
+        case startDate = "started_at"
+        case address = "address"
+        case ownerDisplayName = "owner_display_name"
+    }
+}
--- LaboSwiftUI/View/SecondView.swift
+++ LaboSwiftUI/View/SecondView.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
+class SecondViewModel: ObservableObject {
+    @Published var eventData: [Event] = []
+//    @Published var eventData: [Event] = mockEventsData
+    
+    // connpass's event search API
+    private let urlLink = "https://connpass.com/api/v1/event/?keyword=YUMEMI.swift"
+
+    init() {
+        fetchEventData()
+    }
+
+    func fetchEventData() {
+        URLSession.shared.dataTask(with: URL(string: urlLink)!) { (data, response, error) in
+            guard let data = data else { return }
+            let decoder: JSONDecoder = JSONDecoder()
+            do {
+                let searchedResultData = try decoder.decode(StudyGroup.self, from: data)
+                DispatchQueue.main.async {
+                    self.eventData = searchedResultData.events.reversed()
+                }
+            } catch {
+                print("json convert failed in JSONDecoder. " + error.localizedDescription)
+            }
+        }.resume()
+    }
+}
+
 struct SecondView: View {
+    @ObservedObject var viewModel = SecondViewModel()
+    
     var body: some View {
-        Text("Second")
+        NavigationView {
+            List(viewModel.eventData) { event in
+                Text("\(event.title)")
+            }
+            .navigationBarTitle(Text("YUMEMI.swift一覧"))
+        }
     }
 }
 
