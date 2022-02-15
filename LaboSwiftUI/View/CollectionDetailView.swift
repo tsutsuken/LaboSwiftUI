72a5e7e16c82593b81e732d77acca3a5d6a46063
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-02-15 21:52:54 +0900

    feat: display collection info

--- LaboSwiftUI/View/CollectionDetailView.swift
+++ LaboSwiftUI/View/CollectionDetailView.swift
@@ -12,8 +12,36 @@ struct CollectionDetailView: View {
     
     var body: some View {
         VStack {
-            Text("name: \(collection.name ?? "")")
-            Text("imageUrl: \(collection.imageUrl ?? "")")
+            AsyncImage(url: URL(string: collection.imageUrl ?? "")) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: 96, maxHeight: 96)
+                    .clipShape(Circle())
+            } placeholder: {
+                Image(systemName: "photo.circle")
+                    .resizable()
+                    .frame(width: 96, height: 96, alignment: .center)
+                    .clipShape(Circle())
+            }
+            Text("\(collection.name ?? "")")
+                .font(.title)
+            Text("\(collection.description ?? "")")
+                .font(.caption)
+            if let stats = collection.stats {
+                Text("\(stats.totalSupply) items")
+                    .font(.headline)
+                Text("\(stats.numOwners) owners")
+                    .font(.headline)
+                Text("\(stats.averagePrice) average price")
+                    .font(.headline)
+                Text("\(stats.totalVolume) traded")
+                    .font(.headline)
+            }
+            if let contracts = collection.primaryAssetContracts, !contracts.isEmpty {
+                let firstContract = contracts[0]
+                Text("\(firstContract.address)")
+                    .font(.headline)
+            }
         }
     }
 }
